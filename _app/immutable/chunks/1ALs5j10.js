9b904590d9f793f6c767e9a53ff8da42d4811fa7
Muhammad Imran <imran271828@gmail.com>
2026-02-10 22:12:17 +0600

    final commit

---
+++ _app/immutable/chunks/1ALs5j10.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1742gt5?.base??"/bgb",t=globalThis.__sveltekit_1742gt5?.assets??s;export{t as a,s as b};
--- _app/immutable/chunks/D5zOl839.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_19r5b8b?.base??"/bgb",b=globalThis.__sveltekit_19r5b8b?.assets??s;export{b as a,s as b};
